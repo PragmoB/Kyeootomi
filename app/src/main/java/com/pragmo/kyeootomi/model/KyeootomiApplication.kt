9ae81a1cfef882b6be8417f084513d1bcf533248
PragmoB <peullaegeumo@gmail.com>
2024-04-15 20:20:21 +0900

    bug fix : require RECEIVER_EXPORTED in tiramisu and above

--- app/src/main/java/com/pragmo/kyeootomi/model/KyeootomiApplication.kt
+++ app/src/main/java/com/pragmo/kyeootomi/model/KyeootomiApplication.kt
@@ -3,6 +3,7 @@ package com.pragmo.kyeootomi.model
 import android.app.Application
 import android.app.DownloadManager
 import android.content.IntentFilter
+import android.os.Build
 import android.os.Environment
 import androidx.lifecycle.ProcessLifecycleOwner
 import com.pragmo.kyeootomi.model.data.Collection
@@ -15,7 +16,11 @@ class KyeootomiApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        registerReceiver(downloadReceiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+            registerReceiver(downloadReceiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE), RECEIVER_NOT_EXPORTED)
+        else
+            registerReceiver(downloadReceiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+
         Item.filesDir = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)!!
 
         ProcessLifecycleOwner.get().lifecycle.addObserver(KyeootomiLifecycleObserver(applicationContext))
